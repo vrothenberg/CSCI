702a42667a07764514eda3d469ea724beca3b77c
vrothenberg <vrothenberg@gmail.com>
2019-05-28 16:46:53 -0700

    HackerRank

--- CSCI/main.cpp
+++ CSCI/main.cpp
@@ -6,7 +6,15 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+int saveThePrisoner(int n, int m, int s) {
+
+  return s + ((m % n) - 1);
+
+
+}
+
 int main(){
+  /*
   cout.setf(std::ios::showpoint|std::ios::fixed);
   cout.precision(10);
 
@@ -14,6 +22,8 @@ int main(){
   cout << kPI;
 
   cout << "Hullo";
+  */
+  cout << saveThePrisoner(999999999, 29010, 1);
 
   return 0;
 }
---
+++ Projects/HackerRank/main.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the saveThePrisoner function below.
+// n : number of prisoners
+// m : number of sweets
+// s : starting chair number
+int saveThePrisoner(int n, int m, int s) {
+
+  int prisoner = (s + (m % n) - 1) % n;
+  return prisoner;
+
+
+}
+
+
+
+int main()
+{
+  int s = 5;
+  int q = 4;
+  int k = 7;
+  int i = (s - abs((k % s) - q)) % s;
+  cout << i;
+
+/*
+  cout << "Output   : " << saveThePrisoner(208526924, 756265725, 150817879) << endl;
+  cout << "Expected : " << 72975907;
+
+
+
+  /*
+  string line;
+    ofstream fout("output.txt");
+    cout << "Begin.\n";
+    ifstream fin ("input.txt");
+    int t;
+    if (fin.is_open())
+    {
+      //cout << t << endl;
+      string nms_temp;
+      while ( getline (fin, nms_temp) )
+      {
+        //cout << nms_temp << endl;
+
+        vector<string> nms = split_string(nms_temp);
+        for (int i = 0; i < nms.size(); i++) {
+          cout << nms[i] << " ";
+        }
+        cout << endl;
+        if (nms.size()>1)
+        {
+          int n = std::stoi(nms[0]);
+
+          int m = std::stoi(nms[1]);
+
+          int s = std::stoi(nms[2]);
+
+          int result = saveThePrisoner(n, m, s);
+          cout << "Result : " << result << endl;
+
+          fout << result << "\n";
+        }
+
+      }
+      fin.close();
+    }
+    fout.close();
+*/
+  return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
