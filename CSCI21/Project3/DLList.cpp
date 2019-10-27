84ae4a35453b0d94ef65cd273887ffe4402cd645
vrothenberg <vrothenberg@gmail.com>
2019-10-27 14:33:07 -0700

    Project 3 Revisions

--- CSCI21/Project3/DLList.cpp
+++ CSCI21/Project3/DLList.cpp
@@ -1,13 +1,13 @@
 #include "DLList.h"
 
 Handler::Handler() {
-  cout << "NO FILE PROVIDED\n";
+  cerr << "NO FILE PROVIDED\n";
 };
 
 Handler::Handler(string file) {
   this->file = file;
   this->HandleDLList = nullptr;
-  //this->ReadFile(file);
+  this->ReadFile(file);
 };
 
 Handler::~Handler() {
@@ -15,8 +15,6 @@ Handler::~Handler() {
 };
 
 void Handler::ReadFile(string file) {
-  //if (file == "") file = this->file;
-
   std::ifstream fin(file);
   string line, token, op;
 
@@ -27,32 +25,26 @@ void Handler::ReadFile(string file) {
       try {
         vector<string> opLine;
         int pos = line.find(delim);
+
+        // Loop at least once to parse input line
         do {
           pos = line.find(delim);
           op = line.substr(0,pos);
           opLine.push_back(op);
           line = line.substr(pos+delim.size());
         } while (pos!=-1 );
-
+        // Pass vector of operation and operand to Operation function
         Operation(opLine);
-
-        /*
-        for(string s : opLine){
-          cout << s << " ";
-        }
-        cout << endl;
-        */
-
       } catch (const std::exception &e) {
         // Shouldn't occur normally
-        cout << e.what() << endl;
+        cerr << e.what() << endl;
       }
-
     }
   }
 };
 
 void Handler::Operation(vector<string> opLine) {
+  string listNull = "MUST CREATE LIST INSTANCE\n";
   char op = opLine[0][0];
   int num = 0;
   if (opLine.size() == 2) num = stoi(opLine[1]);
@@ -64,16 +56,17 @@ void Handler::Operation(vector<string> opLine) {
     case 'C':
       // Create dynamic list instance
       if (this->HandleDLList == nullptr) this->Create();
-      else cout << "LIST ALREADY EXISTS\n";
+      else {
+        this->HandleDLList->Clear();
+        cout << "LIST CREATED\n";
+      };
       break;
     case 'X':
       // Clear current list instance of contents
       if (this->HandleDLList) {
         this->HandleDLList->Clear();
         cout << "LIST CLEARED\n";
-      } else {
-        cout << "MUST CREATE LIST INSTANCE\n";
-      }
+      } else cerr << listNull;
       break;
     case 'D':
       // Delete dynamic list and set to nullptr
@@ -82,76 +75,73 @@ void Handler::Operation(vector<string> opLine) {
         delete this->HandleDLList;
         this->HandleDLList = nullptr;
         cout << "LIST DELETED\n";
-      } else cout << "MUST CREATE LIST INSTANCE\n";
-
+      } else cerr << listNull;
       break;
     case 'I':
       // Insert number into list (sorted)
       if (this->HandleDLList) this->HandleDLList->Insert(num);
-      else cout << "MUST CREATE LIST INSTANCE\n";
+      else cerr << listNull;
       break;
     case 'F':
       // Add number to front of list
       if (this->HandleDLList) this->HandleDLList->Front(num);
-      else cout << "MUST CREATE LIST INSTANCE\n";
+      else cerr << listNull;
       break;
     case 'B':
       // Add number to back of list
       if (this->HandleDLList) this->HandleDLList->Back(num);
-      else cout << "MUST CREATE LIST INSTANCE\n";
+      else cerr << listNull;
       break;
     case 'E':
       // Eliminate all occurrences of number from list
       if (this->HandleDLList) this->HandleDLList->EliminateAll(num);
-      else cout << "MUST CREATE LIST INSTANCE\n";
+      else cerr << listNull;
       break;
     case 'R':
       // Remove first occurrence of number from the list
       if (this->HandleDLList) this->HandleDLList->RemoveFirst(num);
-      else cout << "MUST CREATE LIST INSTANCE\n";
+      else cerr << listNull;
       break;
     case 'G':
       // Get number from the list
       if (this->HandleDLList) this->HandleDLList->GetNum(num);
-      else cout << "MUST CREATE LIST INSTANCE\n";
+      else cerr << listNull;
       break;
     case 'A':
       // Return contents of head node
       if (this->HandleDLList) this->HandleDLList->HeadContents();
-      else cout << "MUST CREATE LIST INSTANCE\n";
+      else cerr << listNull;
       break;
     case 'Z':
       // Return contents of tail node
       if (this->HandleDLList) this->HandleDLList->TailContents();
-      else cout << "MUST CREATE LIST INSTANCE\n";
+      else cerr << listNull;
       break;
     case 'T':
       // Pop the head node
       if (this->HandleDLList) this->HandleDLList->PopHead();
-      else cout << "MUST CREATE LIST INSTANCE\n";
+      else cerr << listNull;
       break;
     case 'K':
       // Pop the tail node
       if (this->HandleDLList) this->HandleDLList->PopTail();
-      else cout << "MUST CREATE LIST INSTANCE\n";
+      else cerr << listNull;
       break;
     case 'N':
       // Return the size of the list
       if (this->HandleDLList) this->HandleDLList->ListSize();
-      else cout << "MUST CREATE LIST INSTANCE\n";
+      else cerr << listNull;
       break;
     case 'P':
       // Print all items in the list
       if (this->HandleDLList) this->HandleDLList->PrintList();
-      else cout << "MUST CREATE LIST INSTANCE\n";
+      else cerr << listNull;
       break;
     default:
       cout << "Invalid input.  Shouldn't be here.\n";
       break;
   }
 };
-
-
 
 // Create dynamic list instance
 void Handler::Create() {
@@ -164,12 +154,10 @@ template <typename T>
 DLList<T>::DLList() {
   //Node<T>::created = 0;
   //Node<T>::destroyed = 0;
-
   this->head = nullptr;
   this->tail = nullptr;
   this->size = 0;
 };
-
 
 // Destructor
 template <typename T>
@@ -193,15 +181,6 @@ void DLList<T>::Clear() {
   this->size = 0;
 };
 
-
-// Delete the dynamic list instance and set to nullptr
-template <typename T>
-void DLList<T>::Delete() {
-  this->Clear();
-};
-
-// P 0  N   P  1  N   P 2  N
-// n 10 1   0 20  2   1 30 n
 
 // Insert number into list (sorted)
 template <typename T>
@@ -240,8 +219,6 @@ void DLList<T>::Insert(T value) {
       }
       current = current->next;
     }
-    //delete current;
-    //current = nullptr;
   }
   this->size++;
   cout << "VALUE " << value << " INSERTED\n";
@@ -283,10 +260,8 @@ void DLList<T>::EliminateAll(T value) {
   if(this->size == 0) throw std::logic_error("LIST EMPTY");
   Node<T>* current = this->head;
   while(current != nullptr) {
-    //cout << "CURRENT VALUE: " << current->value << endl;
     if (current->value == value) {
       // Delete Node
-      //cout << "DELETING NODE\n";
       Node<T>* temp = current;
       current = current->next;
       if (temp->prev != nullptr) temp->prev->next = current;
@@ -389,7 +364,7 @@ void DLList<T>::ListSize() {
 template <typename T>
 void DLList<T>::PrintList() {
   Node<T>* current = this->head;
-  if (this->size==0) cout << "LIST EMPTY";
+  if (this->size==0) throw std::logic_error("LIST EMPTY");
   while(current != nullptr) {
     cout << current->value;
     if (current->next != nullptr) cout << ",";
--- CSCI21/Project3/Project3.cpp
+++ CSCI21/Project3/Project3.cpp
@@ -27,7 +27,6 @@ int main(int argc, char* argv[]) {
   }
 
   Handler DoubleLinkedList(file);
-  DoubleLinkedList.ReadFile(file);
 
   return 0;
 }
